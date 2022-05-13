a846f08e93377175aafee8f5ecb212073cb47d54
Мухаммадхаджи Даудов <muhammadhadzidaudov@Mac-Muhammadhadzi.local>
2022-05-13 22:56:53 +0300

    first commit

---
+++ File.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  project
+//
+//  Created by Мухаммадхаджи Даудов on 22.04.2022.
+//
+
+import Foundation
+hdhhdh
+d,hdhhdhld
+dlld
+d.,
+
---
+++ main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  project
+//
+//  Created by Мухаммадхаджи Даудов on 21.04.2022.
+//
+
+import Foundation
+
+/*var a = 10
+a = 11
+a += 1
+print(a)
+
+for a in 1...10{
+    print(a)
+}
+var u = 10
+
+while u > 0{
+    print(u)
+    u -= 1
+}*/
+let input = Int(readLine()!)!
+
+switch input {
+case 2...10:
+    print("fff")
+default: break
+}
